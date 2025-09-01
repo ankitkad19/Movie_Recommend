8f58dd2e94cfa195c8994b40cac5ea53b82ae98a
Ankit Kadwe <119792381+ankitkad19@users.noreply.github.com>
2025-09-01 14:39:31 +0530

    Add files via upload
    added pkl file and updated code

--- recommendation_system/app.py
+++ recommendation_system/app.py
@@ -1,32 +1,49 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import os
+import requests
+
+
+def download_similarity(url, filename):
+    st.info(f"Downloading {filename} from {url} ...")
+    response = requests.get(url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
+    st.success(f"{filename} downloaded.")
 
 
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    index = movies[movies["title"] == movie].index[0]
+    distances = sorted(
+        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1]
+    )
     recommended_movies = []
     for i in distances[1:6]:
         recommended_movies.append(movies.iloc[i[0]].title)
 
     return recommended_movies
 
-movies_dict = pickle.load(open('movie_list.pkl', 'rb'))
+
+SIMILARITY_PATH = "similarity.pkl"
+SIMILARITY_URL = (
+    "https://drive.google.com/uc?export=download&id=1ehXOlYlvz-_gUEmNzfT3Mf2zcv4mQ6Sy"
+)
+
+if not os.path.exists(SIMILARITY_PATH):
+    download_similarity(SIMILARITY_URL, SIMILARITY_PATH)
+
+movies_dict = pickle.load(open("movie_list.pkl", "rb"))
 movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open(SIMILARITY_PATH, "rb"))
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+# movies.head()['title']
+st.title("Movie Recommendation System")
 
-movies.head()['title']
-st.title('Movie Recommendation System')
+selected_movie_name = st.selectbox("Enter the movie you like:", movies["title"].values)
 
-
-selected_movie_name = st.selectbox(
-    'Enter the movie you like:',
-    movies['title'].values)
-
-if st.button('Recommend'):
+if st.button("Recommend"):
     recommendations = recommend(selected_movie_name)
-    st.write('Recommendations for:', selected_movie_name)
+    st.write("Recommendations for:", selected_movie_name)
     for movie in recommendations:
         st.write(movie)
